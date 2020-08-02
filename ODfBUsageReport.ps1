3c057c361fddcee1f4432c811038bd6e1723bcf6
Jordon Threadgill <47824787+jordonthreadgill@users.noreply.github.com>
2020-08-01 22:51:42 -0500

    Add files via upload
    I know most of us admins have a script lying around that'll pull ODfB usage stats.  MS released a new switch into their standard module which allowed me to rewrite my usual (and much more complicated) report to something much faster than before, and it shortens the final output to the main details that decission makers need to see.  I have left this script very open in a way that you can easily add more properties to be shown.  Overall, this is just a revamp of something that's already been done, just in a new way. 
    
    Prereqs:
    1.  You need to be able to already connect to both MSOnline and SharePoint Online with PowerShell (you need both modules installed and working before you can run this)
    
    2.  You just need to have Global Admin or SharePoint Admin rights to run this successfully.
    
     
    
    Enjoy.
    
    (https://gallery.technet.microsoft.com/NEW-OneDrive-ODfB-for-dcf7fbd3)

---
+++ ODfBUsageReport.ps1
@@ -0,0 +1,35 @@
+﻿
+
+$base = $env:USERPROFILE
+$desktop = "$base\Desktop"
+$date = (Get-Date).ToString('MM-dd-yyyy')
+
+$domains = Get-MsolDomain 
+$default = $domains | ? {$_.isdefault -eq $true} | select -expand Name
+
+$Sites = Get-SPOSite -IncludePersonalSite $true
+
+$personalSites = $Sites | ? {$_.template -eq "SPSPERS#10"}
+$usersCount = $personalSites.count
+$report = New-Object -TypeName System.Collections.Generic.List[PSCustomObject]
+$i = 0
+foreach ($u in $personalSites)
+{
+    $LastContentModifiedDate = $u.LastContentModifiedDate
+    $status = $u.Status
+    $StorageUsageCurrent = $u.StorageUsageCurrent
+    $url = $u.Url
+    $owner = $u.Owner
+    $StorageQuota = $u.StorageQuota
+    $StorageQuotaWarningLevel = $u.StorageQuotaWarningLevel
+    $ResourceQuota = $u.ResourceQuota
+    $Title = $u.Title
+    $SharingCapability = $u.SharingCapability
+
+    $object = [pscustomobject]@{Owner = $owner; Title = $Title; LastContentModifiedDate = $LastContentModifiedDate; Url = $url; Status = $status; `
+        StorageUsageCurrent = $StorageUsageCurrent; StorageQuota = $StorageQuota; SharingCapability = $SharingCapability}
+
+    $report.Add($object)
+}
+
+$report | Export-Csv -NoTypeInformation -Path "$desktop\$default ODfB Usage Report $date.csv"
