52b625a7f5477e17b382d1c83c00233e2b0a1444
Jordon Threadgill <47824787+jordonthreadgill@users.noreply.github.com>
2020-08-01 22:45:24 -0500

    Add files via upload
    This version of my script offers two solutions: 1) bulk user loop, and 2) single user loop.  Lastly, you can add an additional site admin for the purpose of completing a migration.
    
    Older version of my script was published here (https://docs.microsoft.com/en-us/onedrive/pre-provision-accounts).

---
+++ Pre-Provision ODfB Sites.ps1
@@ -0,0 +1,39 @@
+﻿# pre-provision ODfB sites
+
+$users = get-msoluser -All | ? {$_.islicensed -eq $true}
+$count = $users.count
+$list = @()
+$i = 0
+$ii = 0
+
+#Option 1
+# Loop of users, in "bulk" for allowed user max - 199/200
+Function Option-1($optionOne){
+    foreach ($u in $users){
+        $i++; $ii++; write-host "$i of $count"
+
+        if ($ii -lt 1){
+            $upn = $u.userprincipalname
+            $list += $upn
+        }
+        if ($ii -gt 1){
+            Request-SPOPersonalSite -UserEmails $list 
+            Start-Sleep -Milliseconds 655
+            $list = @()
+            $ii = 0
+        }
+    }
+    Request-SPOPersonalSite -UserEmails $list -ErrorAction Continue
+}
+Option-1
+
+#Option 2
+# Loops through each end user. [This will take longer]
+Function Option-2($optionTwo){
+    foreach ($u in $users) {Request-SPOPersonalSite -UserEmails $($u.UserPrincipalName); Start-Sleep -Milliseconds 655}
+}
+Option-2
+
+# Add additional ODfB Owner for purpose of completing a migration
+$admin = Read-Host "Enter the Admin account that is completing the data migration..." 
+Get-SPOSite -Limit all -IncludePersonalSite $true -erroraction silentlycontinue | % {Set-SPOUser -site $($_.Url) -LoginName $admin -IsSiteCollectionAdmin:$true -ErrorAction SilentlyContinue; start-sleep -milliseconds 110}
