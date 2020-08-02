552162a3801c127be31561ea52d212694d79ab7d
Jordon Threadgill <47824787+jordonthreadgill@users.noreply.github.com>
2020-08-01 22:48:56 -0500

    Add files via upload
    This script will provide you the most amount of data on Mobile Devices connected to your Office 365 environment, from a very base level, and it will place that report into a CSV file on your desktop.  Mobile Device Management and Azure based reporting are a completely different game.  This is strictly the Mobile Device details from Exchange Online.  It is good stuff though.  Enjoy.
    (https://gallery.technet.microsoft.com/Office-365-Mobile-Device-1523900f)

---
+++ MobileDeviceReport.ps1
@@ -0,0 +1,45 @@
+﻿# Mobile Device Report
+
+$base = $env:USERPROFILE
+$desktop = "$base/Desktop"
+$date = (Get-Date).ToString('MM-dd-yyyy')
+$outpath = "$desktop\MobileReport $date.csv"  ##  change this to whatever you want
+
+$mobileReport = @()
+
+$mailboxes = Get-Mailbox -ResultSize unlimited
+$mailboxesCount = $mailboxes.count
+
+$licensedMailboxes = $mailboxes | ? { $_.RecipientTypeDetails -eq "UserMailbox" }
+
+foreach ($m in $licensedMailboxes)
+{
+    $alias = $m.alias
+    $upn = $m.PrimarySmtpAddress
+
+    $Devices = Get-MobileDevice -Mailbox $upn | select Identity
+    
+    $mobileDevices = @()
+    foreach ($mobile in $devices)
+    {
+        $mobileDevices += Get-MobileDevice -Identity $mobile.identity | Select FriendlyName,DeviceID,DeviceOS,DeviceModel,isDisabled,Identity,GUID,WhenChanged
+    }
+
+        foreach ($device in $mobileDevices)
+        {
+            $id = $device.Identity
+
+            $mobileStats = Get-MobileDeviceStatistics -Identity $id | Select DeviceType,LastSyncAttemptTime,LastSuccessSync,isRemoteWipeSupported,LastDeviceWipeRequestor,LastAccountOnlyDeviceWipeRequestor,DeviceAccessState
+
+            $mobileReport += New-Object psobject -Property @{ UserPrincipalName = $upn; DeviceType = $mobileStats.DeviceType; FriendlyName = $device.FriendlyName; `
+            DeviceID = $device.DeviceID; DeviceModel = $device.DeviceModel; DeviceOS = $device.DeviceOS; IsDisabled = $device.isDisabled; `
+            IsRemoteWipeSupported = $mobileStats.isremotewipesupported; WhenChanged = $device.whenchanged; LastSyncAttemptTime = $mobileStats.lastsyncattempttime; `
+            LastSuccessSync = $mobileStats.lastsuccesssync; LastDeviceWipeRequest = $mobileStats.lastdevicewiperequestor; DeviceAccessState = $mobileStats.DeviceAccessState; `
+            GUID = $device.GUID }
+        }
+}
+
+$mobileReport | select UserPrincipalName,DeviceType,FriendlyName,DeviceID,DeviceModel,DeviceOS,IsDisabled,IsRemoteWipeSupported,WhenChanged,LastSyncAttemptTime,LastSuccessSync,LastDeviceWipeRequest,DeviceAccessState,GUID | `
+Export-Csv -NoTypeInformation -Path $outpath
+
+
